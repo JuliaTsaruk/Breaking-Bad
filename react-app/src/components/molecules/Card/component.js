3538980c268fd5d230ba55d405fef6f32afe311d
Yuliya Tsaruk <yuliyatsaruk@yandex.ru>
2021-09-06 20:23:19 +0300

    feature/React-4: code was cleaned

--- react-app/src/components/molecules/Card/component.js
+++ react-app/src/components/molecules/Card/component.js
@@ -1,19 +1,11 @@
 import React from "react";
 import style from "./styles.module.scss";
-//import {useDispatch} from "react-redux";
-import { change } from "../../../store/actions/test";
 
 const Card = ({ img, status, name, birthday }) => {
-  // const dispatch = useDispatch()
-  
-
-  // const changeAction = () => {
-  //   dispatch(change())
-  // }
   return (
     <div className={style.card}>
       <div className={style.image}>
-        <img onClick={change} src={img} alt="card-img" />
+        <img src={img} alt="card-img" />
       </div>
       <div className={style.info}>
         <p className={style.status}>{status}</p>
