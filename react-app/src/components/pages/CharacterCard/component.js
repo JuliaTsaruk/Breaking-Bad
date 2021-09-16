b614c519fe51d79eb27abf5fbcb6a63515523798
Yuliya Tsaruk <yuliyatsaruk@yandex.ru>
2021-09-16 18:29:51 +0300

    feature/React-6: was added react-router

---
+++ react-app/src/components/pages/CharacterCard/component.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CharacterCard = () => {
+  return <div>CharacterCard page</div>;
+};
+
+export default CharacterCard;
---
+++ react-app/src/components/pages/CharactersList/component.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CharactersList = () => {
+  return <div> CharactersList page</div>;
+};
+
+export default CharactersList;
